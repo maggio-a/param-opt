b28f2aab5cf007ce9c3377c7a91bfb7887ae6388
Andrea Maggiordomo <mggndr89@gmail.com>
2018-07-23 16:16:31 +0200

    fixed dcp solver

--- src/dcp_solver.h
+++ src/dcp_solver.h
@@ -10,13 +10,13 @@
 #include <Eigen/OrderingMethods>
 
 #include "mesh.h"
-#include "vertex_position.h"
+#include "mesh_attribute.h"
 
 #include <vcg/complex/complex.h>
 
 /*
  * Discrete Conformal Parameterization
- * Reference: Desburn, Meyer, Alliez - Intrinsic parameterizations of surface meshes
+ * Reference: Desbrun, Meyer, Alliez - Intrinsic parameterizations of surface meshes
  * Parameterize a mesh by minimizing the Dirichlet's energy of the uv->3D map
  */
 template <typename MeshType>
@@ -67,14 +67,11 @@ public:
         }
     }
 
-    bool Solve(float lambda = 1.0f, float mi = 0.0f)
+    bool Solve()
     {
-        return Solve(DefaultVertexPosition<MeshType>{}, lambda, mi);
-    }
-
-    template <typename VertexPosFct>
-    bool Solve(VertexPosFct VertPos, float lambda = 1.0f, float mi = 0.0f)
-    {
+        assert(HasTargetShapeAttribute(mesh));
+        auto targetShape = GetTargetShapeAttribute(mesh);
+        using namespace vcg;
         // Mark border vertices
         tri::UpdateTopology<MeshType>::FaceFace(mesh);
         tri::UpdateFlags<MeshType>::VertexBorderFromFaceAdj(mesh);
@@ -95,7 +92,7 @@ public:
             const int bestAxis = box.MaxDim();
             for (auto& f : mesh.face) {
                 for (int i = 0; i < 3; ++i) {
-                    Coord3D vpos = VertPos(&f, i);
+                    Coord3D vpos = f.cP(i);
                     if (vpos[bestAxis] <= box.min[bestAxis]) v0 = f.V(i);
                     if (vpos[bestAxis] >= box.max[bestAxis]) v1 = f.V(i);
                 }
@@ -108,7 +105,7 @@ public:
                 if(vv.P()[bestAxis] >= mesh.bbox.max[bestAxis]) v1 = &vv;
             }*/
 
-            assert( (v0!=v1) && v0 && v1);
+            assert((v0!=v1) && v0 && v1);
             AddConstraint(v0, CoordUV{0,0});
             AddConstraint(v1, CoordUV{1,1});
         }
@@ -124,19 +121,19 @@ public:
             for (int i = 0; i < 3; ++i) if (constraints.find(f.V(i)) == constraints.end()) {
                 // indices are given clockwise to avoid flipped parameterizations
                 VertexPointer vi = f.V(i);
-                Coord3D pi = VertPos(&f, i);
+                Coord3D pi = targetShape[f][i];
 
                 VertexPointer vj = f.V((i+2)%3);
-                Coord3D pj = VertPos(&f, (i+2)%3);
+                Coord3D pj = targetShape[f][(i+2)%3];
 
                 VertexPointer vk = f.V((i+1)%3);
-                Coord3D pk = VertPos(&f, (i+1)%3);
+                Coord3D pk = targetShape[f][(i+1)%3];
 
                 ScalarType alpha_ij = std::max(vcg::Angle(pi - pk, pj - pk), eps); // angle at k
                 ScalarType alpha_ik = std::max(vcg::Angle(pi - pj, pk - pj), eps); // angle at j
 
-                ScalarType weight_ij = lambda * (std::tan(M_PI_2 - alpha_ij)) + mi * (std::tan(M_PI_2 - alpha_ik) / (vi->P() - vj->P()).Norm());
-                ScalarType weight_ik = lambda * (std::tan(M_PI_2 - alpha_ik)) + mi * (std::tan(M_PI_2 - alpha_ij) / (vi->P() - vk->P()).Norm());
+                ScalarType weight_ij = std::tan(M_PI_2 - alpha_ij);
+                ScalarType weight_ik = std::tan(M_PI_2 - alpha_ik);
 
                 // Signs should be inverted wrt to the partial derivatives of the energy, but are the same
                 // as the matrix expression on the paper
@@ -164,18 +161,23 @@ public:
         // Prepare to solve
 
         // Allocate matrix and constants vector
-        const int n = vn * 2;
+        const int n = (vn + constraints.size()) * 2;
         Eigen::SparseMatrix<double,Eigen::ColMajor> A(n, n);
         Eigen::VectorXd b(Eigen::VectorXd::Zero(n));
 
         // Add fixed vertices coefficients
+        int k = vn * 2;
         for (auto &c : constraints) {
             VertexPointer vp = c.first;
             CoordUV uv = c.second;
-            coeffList.push_back(Td(U(vp), U(vp), 1));
-            b[U(vp)] = uv[0];
-            coeffList.push_back(Td(V(vp), V(vp), 1));
-            b[V(vp)] = uv[1];
+            coeffList.push_back(Td(U(vp), k, 1));
+            coeffList.push_back(Td(k, U(vp), 1));
+            b[k] = uv[0];
+            k++;
+            coeffList.push_back(Td(V(vp), k, 1));
+            coeffList.push_back(Td(k, V(vp), 1));
+            b[k] = uv[1];
+            k++;
         }
 
         // Initialize matrix
@@ -187,18 +189,10 @@ public:
             std::cout << "Matrix not symmetric" << std::endl;
         }
 
-        // Solve TODO find out why the conjugate gradient fails with x = NaNs
-        //Eigen::ConjugateGradient<Eigen::SparseMatrix<double>, Eigen::Lower|Eigen::Upper> solver;
-        //Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>> solver;
-
-        //Eigen::BiCGSTAB<Eigen::SparseMatrix<double>, Eigen::IncompleteLUT<double>> solver;
-        Eigen::SparseLU<Eigen::SparseMatrix<double,Eigen::ColMajor>, Eigen::COLAMDOrdering<Eigen::SparseMatrix<double>::StorageIndex>> solver;
+        Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>> solver;
 
         A.makeCompressed();
         solver.analyzePattern(A);
-        //solver.setMaxIterations(10);
-        //solver.setTolerance(1e-8);
-
         solver.compute(A);
         if (solver.info() != Eigen::Success) {
             std::cout << "Factorization failed" << std::endl;
@@ -206,9 +200,6 @@ public:
         }
 
         Eigen::VectorXd x = solver.solve(b);
-
-        //std::cout << "#iterations:     " << solver.iterations() << std::endl;
-        //std::cout << "estimated error: " << solver.error()      << std::endl;
 
         if (solver.info() != Eigen::Success) {
             std::cout << "Solving failed" << std::endl;
@@ -221,7 +212,7 @@ public:
             uvBox.Add(CoordUV(ScalarType(x[U(i)]), ScalarType(x[V(i)])));
         }
 
-        float scale = 1.0f / std::max(uvBox.Dim().X(), uvBox.Dim().Y());
+        double scale = 1.0 / std::max(uvBox.Dim().X(), uvBox.Dim().Y());
 
         for (auto &f : mesh.face) {
             for (int i = 0; i < 3; ++i) {
